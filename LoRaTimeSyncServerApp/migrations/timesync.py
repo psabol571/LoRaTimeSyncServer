10712c5245431e087400b634f9ac75864ba848ab
Patrik Sabol <psabol571@gmail.com>
2024-10-08 10:14:20 +0200

    add saveTimeCollection function

--- LoRaTimeSyncServerApp/migrations/timesync.py
+++ LoRaTimeSyncServerApp/migrations/timesync.py
@@ -19,4 +19,29 @@ def initTimeSync(dev_eui, period):
     return time_sync_device
 
 
+def saveTimeCollection(dev_eui, device_time, time_received=timezone.now()):
+    device = TimeSyncInit.objects.get(dev_eui=dev_eui)
+    
+    latest_collection = TimeCollection.objects.filter(dev_eui=dev_eui).order_by('-time_received').first()
+    
+    # calculate nearest period
+    if latest_collection:
+        time_diff = time_received - latest_collection.time_expected
+        periods_passed = round(time_diff.total_seconds() / device.period)
+        time_expected = latest_collection.time_expected + timedelta(seconds=periods_passed * device.period)
+    else:
+        time_diff = time_received - device.first_uplink_expected
+        periods_passed = round(time_diff.total_seconds() / device.period)
+        time_expected = device.first_uplink_expected + timedelta(seconds=periods_passed * device.period)
+    
+    
+    # Create and save the TimeCollection instance
+    time_collection = TimeCollection.objects.create(
+        dev_eui=dev_eui,
+        time_received=time_received,
+        time_expected=time_expected,
+        device_time=device_time
+    )
+    
+    return time_collection
 
