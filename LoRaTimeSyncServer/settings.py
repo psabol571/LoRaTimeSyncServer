6e670fb34c311157cc1b03a72c6f93dcf7ac8e3e
Patrik Sabol <psabol571@gmail.com>
2024-10-08 08:53:15 +0200

    add docker and postgres

--- LoRaTimeSyncServer/settings.py
+++ LoRaTimeSyncServer/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'LoRaTimeSyncServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'loratimesync',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
