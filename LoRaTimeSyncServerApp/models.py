d60abda0ab4bb0e021abeaad1e706b86e643cdd3
Patrik Sabol <psabol571@gmail.com>
2024-10-08 09:36:23 +0200

    add initial models and first migration

--- LoRaTimeSyncServerApp/models.py
+++ LoRaTimeSyncServerApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class TimeSyncInit(models.Model):
+    dev_eui = models.CharField(max_length=16)
+    period = models.IntegerField()
+    first_uplink_expected = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class TimeCollection(models.Model):
+    dev_eui = models.CharField(max_length=16)
+    device_time = models.DateTimeField()
+    time_expected = models.DateTimeField()
+    time_received = models.DateTimeField()
+
+
+class TimeSyncModels(models.Model):
+    dev_eui = models.CharField(max_length=16)
+    a=models.FloatField()
+    b=models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
