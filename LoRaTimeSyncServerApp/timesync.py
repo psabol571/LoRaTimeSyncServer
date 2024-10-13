029dfe6a13aebf848493ce17c0f923ff5f13de44
Patrik Sabol <psabol571@gmail.com>
2024-10-13 13:32:24 +0200

    fix Int to BigInt to store nanoseconds

--- LoRaTimeSyncServerApp/timesync.py
+++ LoRaTimeSyncServerApp/timesync.py
@@ -1,25 +1,24 @@
+import time
 
 from django.utils import timezone
 from datetime import timedelta
 from .models import TimeSyncInit, TimeCollection
 
 def initTimeSync(dev_eui, period):
-    now = timezone.now()
-    first_uplink_expected = now + timedelta(seconds=period)
-    
-    # Round up to the nearest minute
-    first_uplink_expected = first_uplink_expected.replace(second=0, microsecond=0) + timedelta(minutes=1)
+    now = time.time_ns()
+    reserve = 10
+    first_uplink_expected = now + (period + reserve) * (1000**3)  # add period and reserver as nanoseconds
     
     time_sync_device = TimeSyncInit.objects.create(
         dev_eui=dev_eui,
         period=period,
-        first_uplink_expected=first_uplink_expected
+        first_uplink_expected=first_uplink_expected,
     )
-    
-    return time_sync_device
+
+    return now
 
 
-def saveTimeCollection(dev_eui, device_time, time_received=timezone.now()):
+def saveTimeCollection(dev_eui, device_time=timezone.now(), time_received=timezone.now()):
     device = TimeSyncInit.objects.get(dev_eui=dev_eui)
     
     latest_collection = TimeCollection.objects.filter(dev_eui=dev_eui).order_by('-time_received').first()
