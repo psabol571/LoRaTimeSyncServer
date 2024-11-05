9cb4ec36c2376008b988acf535445766a67a9e1d
Patrik Sabol <psabol571@gmail.com>
2024-11-05 11:35:41 +0100

    downlink debug

--- LoRaTimeSyncServerApp/ChirpStackUtils/downlink.py
+++ LoRaTimeSyncServerApp/ChirpStackUtils/downlink.py
@@ -3,9 +3,14 @@ import grpc
 from chirpstack_api import api
 from django.conf import settings
 
+import logging
+logger = logging.getLogger('django')
+
 
 def send_downlink(dev_eui, data):
     data = json.dumps(data)
+    logger.info('data_downlink')
+    logger.info(data)
 
     channel = grpc.insecure_channel(settings.CHIRPSTACK_HOST)
 
@@ -18,5 +23,7 @@ def send_downlink(dev_eui, data):
     req.queue_item.data = bytes(data, encoding='utf-8')
     req.queue_item.dev_eui = dev_eui
     req.queue_item.f_port = 10
+
+    logger.info(req.queue_item.data)
 
     client.Enqueue(req, metadata=auth_token)
