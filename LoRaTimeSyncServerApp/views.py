1fb4e56f13c212ab40eca5ca68716aa3933c66fc
Patrik Sabol <psabol571@gmail.com>
2024-11-24 19:56:50 +0100

    new graph v3

--- LoRaTimeSyncServerApp/views.py
+++ LoRaTimeSyncServerApp/views.py
@@ -193,3 +193,58 @@ def time_difference_graph_v2(request):
         return HttpResponse(buffer.getvalue(), content_type='image/png')
     
     return HttpResponse("No data available", content_type='text/plain')
+
+
+# example usage: localhost:8000/graph-time-diff_v2?time_from=2023-01-01T00:00:00&time_to=2023-12-31T23:59:59&dev_eui=test_dev_eui
+@csrf_exempt
+def time_difference_graph_v3(request):
+    dev_eui = request.GET.get('dev_eui', '')
+    time_from = request.GET.get('time_from', '')
+    time_to = request.GET.get('time_to', '')
+
+    #  Convert time strings to datetime objects
+    time_from = timezone.datetime.fromisoformat(time_from) if time_from else timezone.now() - timedelta(days=7)
+    time_to = timezone.datetime.fromisoformat(time_to) if time_to else timezone.now()
+
+    #  Convert datetime to Unix timestamp in nanoseconds
+    unix_from = time_from.timestamp() * 1e9
+    unix_to = time_to.timestamp() * 1e9
+
+    # Get the last TimeSyncInit record for this experiment
+    sync_init = TimeSyncInit.objects.filter(
+        dev_eui=dev_eui,
+        created_at__lte=time_to
+    ).order_by('-created_at').first()
+
+    # Fetch TimeCollection data
+    collections = TimeCollection.objects.filter(
+        dev_eui=dev_eui,
+        time_received__range=(unix_from, unix_to)
+    ).order_by('time_received')
+
+    if collections and sync_init:
+
+        filtered_data = [(c.time_expected - sync_init.first_uplink_expected, c.time_expected - c.time_received) 
+                     for c in collections 
+                     if (c.time_expected - c.time_received) > 0]
+
+        # x_values represents minutes now
+        x_values = [(x[0]) / (60 * 1e9) for x in filtered_data]
+        time_diffs = [x[1] for x in filtered_data]
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(x_values, time_diffs, 'bo-')
+        plt.xlabel('Time (minutes)')
+        plt.ylabel('Time Difference (nanoseconds)')
+        plt.title(f'Time Difference for Device \'{dev_eui}\' from {time_from} to {time_to}')
+        plt.grid(True)
+
+        # Save the plot to a buffer
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        plt.close()
+
+        return HttpResponse(buffer.getvalue(), content_type='image/png')
+    
+    return HttpResponse("No data available", content_type='text/plain')
