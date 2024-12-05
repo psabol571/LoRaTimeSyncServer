430e1546cf13ab933a22914e0d86523eb06c4334
Patrik Sabol <psabol571@gmail.com>
2024-12-05 14:43:13 +0100

    fix

--- LoRaTimeSyncServerApp/views.py
+++ LoRaTimeSyncServerApp/views.py
@@ -345,7 +345,7 @@ def test_model(request):
         'P': sync_init.period * 1e9 * model.coef_[0],
         'p_micro': int(sync_init.period * 1e9 * model.coef_[0] / 1e3),
         'a_recreated': int(sync_init.period * 1e9 * model.coef_[0] / 1e3) / sync_init.period / 1e6,
-        'pmicro2': (int(sync_init.period * 1e9 * (int(sync_init.period * 1e9 * model.coef_[0] / 1e3) / sync_init.period / 1e6) / 1e3) / sync_init.period / 1e6),
+        'pmicro2': int(sync_init.period * 1e9 * (int(sync_init.period * 1e9 * model.coef_[0] / 1e3) / sync_init.period / 1e6) / 1e3)
     }))
 
 
