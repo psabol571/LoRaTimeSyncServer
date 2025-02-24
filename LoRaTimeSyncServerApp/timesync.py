5995a7f53ac0c1a4bb8487244f0a367b680586d1
Patrik Sabol <psabol571@gmail.com>
2025-02-24 14:14:06 +0100

    fix ofset downlink condition

--- LoRaTimeSyncServerApp/timesync.py
+++ LoRaTimeSyncServerApp/timesync.py
@@ -112,15 +112,22 @@ def perform_sync(dev_eui):
     logger.info("Collections length")
     logger.info(len(collections))
 
-    if len(collections) == 1:
+    # perform sync only when you have at least MIN_N records of data
+    MIN_N = 15
+    if len(collections) <= MIN_N:
         # Calculate the offset for the first uplink
         first_collection = collections[0]
-        offset = first_collection.time_received - first_collection.time_expected
+        offset = first_collection.time_expected - first_collection.time_received
+
+        logger.info(f"offset {offset}")
+
+        tolerance = 0.2 * 1e9
+        if abs(offset) < tolerance:
+            return
         # send offset after first uplink
         return f's,{int(offset)}'  # You can log this or handle it as needed
 
-    # perform sync only when you have at least MIN_N records of data
-    MIN_N = 15
+    
     if len(collections) < MIN_N:
         return
 
