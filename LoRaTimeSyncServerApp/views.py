0180c9097352ae55fbd5e5cc7c3784eefb67e932
Patrik Sabol <psabol571@gmail.com>
2025-03-09 12:01:14 +0100

    add remove outlier option to graph_v2

--- LoRaTimeSyncServerApp/views.py
+++ LoRaTimeSyncServerApp/views.py
@@ -102,7 +102,8 @@ def time_difference_graph(request):
 def time_difference_graph_v2(request):
     dev_eui, time_from, time_to, unix_from, unix_to = get_time_range_params(request)
     error_greater_than_seconds = request.GET.get('e', -1)
-    sync_init, collections = get_sync_data(dev_eui, time_to, unix_from, unix_to, error_greater_than_seconds)
+    remove_outliers = request.GET.get('o', False)
+    sync_init, collections = get_sync_data(dev_eui, time_to, unix_from, unix_to, error_greater_than_seconds, remove_outliers)
 
     if collections and sync_init:
         x_values = [(c.time_expected - sync_init.first_uplink_expected) / (60 * 1e9) for c in collections]
