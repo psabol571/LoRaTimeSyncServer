3e06cbcce085150eca41da07c3da46547502ecdb
Patrik Sabol <psabol571@gmail.com>
2025-03-17 21:26:14 +0100

    add resync

--- LoRaTimeSyncServerApp/timesync.py
+++ LoRaTimeSyncServerApp/timesync.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 from .models import TimeSyncInit, TimeCollection, TimeSyncModels
 from sklearn.linear_model import LinearRegression
 import numpy as np
-
+from .testing_utils import filter_time_diff_outliers
 import logging
 logger = logging.getLogger('django')
 
@@ -61,16 +61,12 @@ def createModel(collections, first_received):
     return model
 
 
-def createModelFromCollections(collections, dev_eui, old_period_ns):
-    # create linear regression model
+def createModelWithOffset(collections, dev_eui, old_period_ns):
     model = createModel(collections, collections[0].time_received)
 
     new_period_ns = int(old_period_ns * model.coef_[0])
     new_period_ms = int((new_period_ns + 500) / 1e3)
-    
-    # offset can be calculated as accumulated error over the period passed + model.b
 
-    # calculate accumulated error over the period passed
     time_diff_ns = collections[len(collections) - 1].time_expected - collections[0].time_expected
     # periods_passed = round(time_diff_ns / old_period_ns)
     # accumulated_error = (new_period_ns - old_period_ns) * periods_passed
@@ -78,19 +74,38 @@ def createModelFromCollections(collections, dev_eui, old_period_ns):
     # accumulated_error = old_period_ns * (model.coef_[0] - 1) * periods_passed
     # accumulated_error = old_period_ns * (model.coef_[0] - 1) * time_diff_ns / old_period_ns
     accumulated_error = time_diff_ns * (model.coef_[0] - 1)
-    offset = accumulated_error + model.intercept_
+    offset = int(accumulated_error + model.intercept_)
 
-    # Save the model parameters
-    model = TimeSyncModels.objects.create(
-        dev_eui=dev_eui,
-        a=model.coef_[0],
-        b=model.intercept_,
-        last_collection_time_received=collections[len(collections) - 1].time_received,
-        new_period_ms=new_period_ms,
-        new_period_ns=new_period_ns,
+    return {
+        'a': model.coef_[0],
+        'b': model.intercept_,
+        'new_period_ns': new_period_ns,
+        'new_period_ms': new_period_ms,
+        'offset': offset,
+        'dev_eui': dev_eui,
+        'last_collection_time_received': collections[len(collections) - 1].time_received,
+    }
+    
+
+def saveModelWithOffset(model_data):
+    return TimeSyncModels.objects.create(
+        dev_eui=model_data['dev_eui'],
+        a=model_data['a'],
+        b=model_data['b'],
+        last_collection_time_received=model_data['last_collection_time_received'],
+        new_period_ns=model_data['new_period_ns'],
+        new_period_ms=model_data['new_period_ms'],
+        offset=model_data['offset'],
     )
 
-    return f's,{int(offset)},{model.new_period_ns}'
+
+def createModelFromCollections(collections, dev_eui, old_period_ns):
+    # create linear regression model
+    model = createModelWithOffset(collections, dev_eui, old_period_ns)
+
+    saveModelWithOffset(model)
+
+    return f's,{model["offset"]},{model["new_period_ns"]}'
 
 
 def syncAfterFirstUplink(collections):
@@ -137,16 +152,24 @@ def existingModelSync(existing_model, MIN_N, MIN_HOURS_FOR_NEW_MODEL):
     collections = TimeCollection.objects.filter(
         dev_eui=existing_model.dev_eui,
         time_received__gt=existing_model.last_collection_time_received, # after the last model creation
-        time_expected__gt=F('time_received') - 1000000000 # error (expected - received) > - 1 second (filters out deep sleep outliers)
     ).order_by('time_received')
+
+    collections = filter_time_diff_outliers(collections)
 
     logger.info(f"existingModelSync - collections lenght: {len(collections)}")
         
-    # perform sync on clockdrift only when you have at least MIN_N records of data
-    if len(collections) < MIN_N:
+    # first, create a model without saving it
+    model = createModelWithOffset(collections, existing_model.dev_eui, existing_model.new_period_ms * 1e3)
+
+    treshold_offset_nanoseconds = 0.09 * 1e9
+
+    # if offset is too small, do not save a new model, we dont need a resync yet
+    if model['offset'] < treshold_offset_nanoseconds:
         return
 
-    return createModelFromCollections(collections, existing_model.dev_eui, existing_model.new_period_ms * 1e3)
+    saveModelWithOffset(model)
+
+    return f's,{model["offset"]},{model["new_period_ns"]}'
 
 
 def perform_sync(dev_eui):
@@ -170,7 +193,7 @@ def perform_sync(dev_eui):
     if existing_model is None:
         return nonExistingModelSync(sync_init, MIN_N)
     else:
-        return # existingModelSync(existing_model, MIN_N, MIN_HOURS_FOR_NEW_MODEL)
+        return existingModelSync(existing_model, MIN_N, MIN_HOURS_FOR_NEW_MODEL)
 
 
 
